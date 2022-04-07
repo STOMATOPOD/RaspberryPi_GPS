a00a3c45fe6c3d7dc6be7df326de9edd8d8dbd3c
RossV <50552933+STOMATOPOD@users.noreply.github.com>
2022-04-06 21:20:59 -0400

    Update bag_to_csv.sh

--- scripts/bag_to_csv.sh
+++ scripts/bag_to_csv.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-# get to the right folder
-D="$(find ~ -name gps_drift 2>/dev/null)"
-cd "$D"
+# constraint: must be named gps_drift
+# get to the working directory
+work_dir="$(find ~ -name gps_drift 2>/dev/null)" && cd $work_dir
 
-rm -Rf csv 
-mkdir csv 
+# get rid of the old output folder & remake
+rm -Rf csv_out
+mkdir csv_out && cd csv_out
 
-# get our python script
-P="$(find . -name bags_to_csv.py 2>/dev/null)"
+# get python script (constraint: must be in the gps_drift dir)
+python_script="$(find $work_dir -name "bag_to_csv.py" 2>/dev/null)"
+echo "SCRIPT    : $python_script"
 
-echo "$P"
+for i in $(find $work_dir -name "m*.bag" 2>/dev/null); do
+    echo "CONVERTING: $i"
+    # start the python script with input file & destination
+    # the python script creates 3 incomplete files
+    python3 $python_script "$i" "$work_dir/csv_out"
+    echo "MERGING   : $i"
+    # collect the 3 incomplete files
+    T="$(ls | grep "time")"
+    P="$(ls | grep "pos")"
+    F="$(ls | grep "fix")"
 
+    # combine them with paste
+    O="$(echo $T | sed 's/.bag_time//')"
 
-# paste m1_gps_2022-02-17-16-30-00.bag_time.csv m1_gps_2022-02-17-16-30-00.bag_pos.csv m1_gps_2022-02-17-16-30-00.bag_fix.csv -d "," > m1_gps_2022-02-17-16-30-00.csv
-# paste m2_gps_2022-02-17-16-30-00.bag_time.csv m2_gps_2022-02-17-16-30-00.bag_pos.csv m2_gps_2022-02-17-16-30-00.bag_fix.csv -d "," > m2_gps_2022-02-17-16-30-00.csv
-# paste m3_gps_2022-02-17-16-30-00.bag_time.csv m3_gps_2022-02-17-16-30-00.bag_pos.csv m3_gps_2022-02-17-16-30-00.bag_fix.csv -d "," > m3_gps_2022-02-17-16-30-00.csv
-# paste m4_gps_2022-02-17-16-30-00.bag_time.csv m4_gps_2022-02-17-16-30-00.bag_pos.csv m4_gps_2022-02-17-16-30-00.bag_fix.csv -d "," > m4_gps_2022-02-17-16-30-00.csv
+    paste $T $P $F -d "," > $O
+    # CLEANUP: remove the incomplete files
+    rm $T $P $F
+    echo "      DONE"
+done
